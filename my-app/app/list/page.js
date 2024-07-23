97578b266c67d0d0232248212f27c1688d1c28f4
minidh <m_dohui59@naver.com>
2024-07-23 15:33:59 +0900

    next_js

--- my-app/app/list/page.js
+++ my-app/app/list/page.js
@@ -1,6 +1,10 @@
 'use client'
+import { useState } from "react"
+
 export default function List () {
     let 상품 = ['Tomatoes', 'Pizza', 'Pasta']
+    let [수량,수량변경] = useState([0,0,0]);
+
     return (
         <div>
             <h4 className="tilte">상품 목록</h4>
@@ -10,8 +14,12 @@ export default function List () {
                         <div className="food" key={i}>
                         <img src={`/food${i}.png`} className="food-img"></img>
                         <h4>{a} 14000원</h4>
-                        <span> 1</span>
-                        <button onClick={()=>{console.log(1)}}>+</button>
+                        <span> {수량[i]} </span>
+                        <button onClick={()=> {
+                            let copy = [...수량];
+                            copy[i]++;
+                            수량변경(copy);
+                        }}>+</button>
                         </div>
                     )
                 })
